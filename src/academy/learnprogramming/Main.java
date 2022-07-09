b7089da027e4bc9ad66c6d618c9ccc95ee8a2bb6
dpetskos <dpetskos@gmail.com>
2022-07-09 14:04:18 +0300

    Test with byte

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -2,5 +2,19 @@ package academy.learnprogramming;
 
 public class Main {
 
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Int min value " + myMinIntValue);
+        System.out.println("Int max value "  + myMaxIntValue);
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Int min value " + myMinByteValue);
+        System.out.println("Int max value "  + myMaxByteValue);
+    }
 }
